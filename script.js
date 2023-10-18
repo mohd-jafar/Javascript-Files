62f3b6af6bbdaea1bb1ed91c31c4424e2743b05f
mohd-jafar <142247641+mohd-jafar@users.noreply.github.com>
2023-10-18 19:56:32 +0530

    Javascript Basics
    Javascript Basics

---
+++ script.js
@@ -0,0 +1,38 @@
+console.log("Ex-2 _number variables")
+
+var num=10
+console.log(num)
+
+var num=-10
+console.log(num)
+
+var num=10.5
+console.log(num)
+
+
+console.log("Ex-3_string variables")
+
+ var str="a"
+ console.log(str)
+
+
+ var str="text"
+ console.log(str)
+
+ var str="Creating String variables"
+ console.log(str)
+
+console.log("Ex-4_boolean variables")
+var hour=true
+var minute=false
+var second="boolean variables contain true,false"
+console.log(hour)
+console.log(minute)
+console.log(second)
+
+
+console.log("Ex-5_array")
+
+var values=[1,"string",true,false,null]
+console.log(values)
+
